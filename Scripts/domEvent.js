a9265df4901d12181dc56f978e934732147a406f
guhanMurugesan <mguhan.rmd@gmail.com>
2016-04-29 21:54:53 +0530

    added indian citizen service PoC

---
+++ Scripts/domEvent.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    $('.voteImg').hover(function () {
+        $(this).css('opacity', '0.7');
+    }, function () {
+        $(this).css('opacity', '1');
+    });
+
+    $('.voteImg').click(function(){
+        $('.voteImg').css('box-shadow','0px 0px 0px 0px black');
+        $(this).css('box-shadow','0px 0px 5px 5px black');
+    });
+
+    $('#goLive').click(function () {
+        window.location.href = liveMapUrl;
+    });
+
+    $('#userMap').click(function () {
+        window.location.href = userMapUrl;
+    });
+});
